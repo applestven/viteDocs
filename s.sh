b88a3613b9daecdc2c4cc25d69058df6d98b2bce
stven apple <honyao@qq.com>
2024-11-01 11:02:04 +0800

    更新连接服务器快捷脚本 ，更新cpe知识

---
+++ s.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 定义 SSH 账号列表
+declare -A ssh_accounts=(
+    ["1"]="apple@192.168.191.214"
+    ["2"]="ubuntu@43.139.236.50"
+    ["3"]="apple@83.229.123.43"
+)
+
+# 显示 SSH 账号列表
+echo "请选择一个 SSH 账号："
+for key in "${!ssh_accounts[@]}"; do
+    echo "$key) ${ssh_accounts[$key]}"
+done
+
+# 获取用户选择
+read -p "请输入编号: " choice
+
+# 检查用户输入是否有效
+if [[ -z "${ssh_accounts[$choice]}" ]]; then
+    echo "无效的选择！"
+    exit 1
+fi
+
+# 获取选中的 SSH 地址
+selected_ssh="${ssh_accounts[$choice]}"
+
+# 执行 SSH 连接
+ssh $selected_ssh
---
+++ 网络/cpe.md
@@ -0,0 +1,19 @@
+## 关于三网频段的知识
+
+[鸡血频段的选择](https://post.smzdm.com/p/an9m9987/) 
+
+## 测速网 
+
+[Speedtest.net](https://www.speedtest.net/)
+
+
+https://www.speedtest.cn/
+
+
+[电信宽带网页测速](https://10000.gd.cn/#/speed)
+
+
+[南京大学测速网](https://test.nju.edu.cn/)
+
+
+[台湾大学测速网](https://www.zhihu.com/tardis/bd/art/529511758?source_id=1001)
