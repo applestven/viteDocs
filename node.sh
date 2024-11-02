a703b16ca612726737c4b99ac6f2512a803bbdc7
stven apple <honyao@qq.com>
2024-11-02 15:09:51 +0800

    添加node.sh 环境安装

---
+++ node.sh
@@ -0,0 +1,15 @@
+## ubuntu系统安装环境
+
+## vim
+
+## nvm 手动安装  默认安装node 20.17.0
+
+## git 
+
+## 新建路径 /home/apple/code/gitee/  /home/apple/code/github/
+
+## 在/home/apple/code/gitee/ 安装若干个项目 在/home/apple/code/github/中安装若干项目
+
+## 安装nginx
+
+
