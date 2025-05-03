01aea14710925f12bdc98a7efe83031fd384927b
apple <common@gmail.com>
2025-05-03 15:07:41 +0800

    feat:share_file部署

--- deploy_sh/zerotier_install_custom.sh
+++ deploy_sh/zerotier_install_custom.sh
@@ -1,7 +1,7 @@
 ## 安装并 加入自定义的plant 节点
 
 ## 1. 安装 ZeroTier
-sudo apt reemove zerotier-one
+curl -s https://install.zerotier.com | sudo bash
 
 sudo apt install zerotier-one
 
@@ -14,10 +14,10 @@ sudo systemctl start zerotier-one
 rm -rf /var/lib/zerotier-one/planet
 
 ## 使用wget下载文件并保存为指定名称和路径
-wget -O /var/lib/zerotier-one/planet "http://83.229.123.43:3000/planet?key=c7b1299681198500"
+wget -O /var/lib/zerotier-one/planet "http://83.229.123.43:3000/planet?key=00dc51157b5b307e"
 
 ## 重启 ZeroTier
 service zerotier-one restart
 
 ## 加入网络
-zerotier-cli join bd404f7ed432d4b8
+zerotier-cli join 00dc51157b5b307e
