2d9dbdba0a42a36312bbb9cb12dd2253fd4cb8dc
appleair <applestven@icloud.com>
2025-06-25 13:27:38 +0800

    build(deploy): 更新 SCP 上传脚本
    - 添加上传文件夹功能
    - 新增多个 SCP 上传命令
    - 删除了无关的 Markdown 文件

--- deploy_sh/scp_upload.sh
+++ deploy_sh/scp_upload.sh
@@ -1,3 +1,9 @@
 scp ./nginx_static.sh root@38.38.250.209:/home/ubuntu/
 
 scp ./1.m4a apple@192.168.0.110:/home/apple/temp/
+
+# 上传文件夹
+scp -r ./test_folder root@192.168.0.110:/home/ubuntu/
+
+
+scp -r ./whisper-service apple@192.168.8.109:/home/apple/code
